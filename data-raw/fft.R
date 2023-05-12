bb5f6858644f55a6a7382a1357dd70a28a7b0cb6
ruthjohnson78 <ruthjohnson78@users.noreply.github.com>
2023-05-12 17:04:06 +0000

    Style code (GHA)

--- data-raw/fft.R
+++ data-raw/fft.R
@@ -1,7 +1,8 @@
 ## code to prepare `fft` dataset goes here
 
 fft_raw <- readxl::read_excel(
-  "O://T&C//BI Consultancy//A - Projects//UoM - SCW - PC CAP//Raw Data//GP FFT Data.xlsx")
+  "O://T&C//BI Consultancy//A - Projects//UoM - SCW - PC CAP//Raw Data//GP FFT Data.xlsx"
+)
 
 
 fft <- fft_raw
