a7e63b63f140c86006105f576a21fd642993af23
Ruth Johnson <59695041+ruthjohnson78@users.noreply.github.com>
2023-05-15 17:25:48 +0100

    Split question numbers out

--- data-raw/gppt.R
+++ data-raw/gppt.R
@@ -22,4 +22,60 @@ gppt <- dplyr::rename(
   year = X13
 )
 
+
+questions <-unique(gppt[c('source_question_number', 'question_number', 'question_description')])
+
+
+library(stringr)
+library(dplyr)
+questions <- questions %>%
+       mutate(
+         question = case_when (
+           questions$question_number == 'Q01' ~ "Ease of getting through to someone at GP practice on the phone",
+           questions$question_number == 'Q04' ~ "Ease of using GP practice's website to look for information or access services",
+           questions$question_number == 'Q16' ~ "Satisfaction with type of appointment offered",
+           questions$question_number == 'Q21' ~ "Overall experience of making an appointment",
+           questions$question_number == 'Q32' ~ "Overall experience of GP practice"
+           )  )
+
+questions <- questions %>%
+        mutate(
+         summary_flag = case_when (
+                str_detect(question_description,"Summary") ~ TRUE,
+                TRUE~ FALSE
+                )
+         )
+
+questions <- questions %>%
+  mutate(
+    confidence_flag = case_when (
+      str_detect(question_description,"confidence") ~ TRUE,
+      TRUE~ FALSE
+    )
+  )
+
+questions <- questions %>%
+        mutate(
+          answers = sapply(strsplit(questions$question_description," - ",fixed=TRUE),tail, 1)
+        )
+
+
+
+questions_summary <-questions %>%
+  filter(questions$summary_flag == TRUE)
+
+questions_summary <-  questions_summary %>%
+  mutate(
+    summary_context = sapply(strsplit(questions_summary$answers," (",fixed=TRUE),tail, 1)
+        )
+
+questions_summary <- gsub("[()]","",questions_summary$summary_context)
+
+
+
+
+
+
 usethis::use_data(gppt, overwrite = TRUE)
+
+
