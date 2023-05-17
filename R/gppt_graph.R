49b2078817f3eda7ec663f8e3c62319e3188b923
Ruth Johnson <59695041+ruthjohnson78@users.noreply.github.com>
2023-05-17 18:29:47 +0100

    GPPT graph development
    Graphs for practices

--- R/gppt_graph.R
+++ R/gppt_graph.R
@@ -6,19 +6,80 @@ devtools::install_github("bbc/bbplot")
 install.packages("tidyverse")
 library(tidyverse)
 library(bbplot)
+library(scwplot)
 
-test <- filter(gppt, practice_code == "K84028", question_number == "Q01", source_question_number == "q3_12")
+####Practice Level Graphs
+##practice_code and question_number to be dependent on the selected item on the app
+gppt_practice_graph <- filter(gppt_final, practice_code == "K84028", question_number == "Q16", summary_flag == FALSE, confidence_flag ==FALSE, total_flag ==FALSE)
 
-bars <- ggplot(test, aes(x = year, y = value)) +
+practice_stack <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = value)) +
   geom_bar(
     stat = "identity",
-    position = "identity",
-    fill = "#1380A1"
+    position = "stack"
   ) +
-  geom_hline(yintercept = 0, size = 1, colour = "#333333") +
-  bbc_style() +
+  geom_hline(yintercept = 0, size = 1, colour="#333333") +
+  theme_scw() +
+  scale_fill_diverging(discrete = TRUE) +
   labs(
-    title = test$question_number,
-    subtitle = test$practice_name
+    title = gppt_practice_graph$question,
+    subtitle = gppt_practice_graph$practice_name,
+    y = 'Number of Patients',
+    x = "Year"
   )
-bars
+practice_stack
+
+practice_perc <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = value)) +
+  geom_bar(
+    stat = "identity",
+    position = "fill"
+  )  +
+  theme_scw() +
+  geom_hline(yintercept = 0, size = 1, colour="#333333") +
+  scale_y_continuous(name = "Percentage of Patients",
+                     labels = scales::label_percent()) +
+  scale_fill_diverging(discrete = TRUE) +
+  labs(
+    title = gppt_practice_graph$question,
+    subtitle = gppt_practice_graph$practice_name,
+    x = "Year"
+  )
+
+practice_perc
+
+####PCN Level Graphs
+##pcn_code and question_number to be dependent on the selected item on the app
+gppt_pcn_graph <- filter(gppt_final, pcn_code == "U36809", question_number == "Q16", summary_flag == FALSE, confidence_flag ==FALSE, total_flag ==FALSE)
+
+pcn_stack <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
+  geom_bar(
+    stat = "identity",
+    position = "stack"
+  ) +
+  geom_hline(yintercept = 0, size = 1, colour="#333333") +
+  theme_scw() +
+  scale_fill_diverging(discrete = TRUE) +
+  labs(
+    title = gppt_pcn_graph$question,
+    subtitle = gppt_pcn_graph$practice_name,
+    y = 'Number of Patients',
+    x = "Year"
+  )
+pcn_stack
+
+pcn_perc <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
+  geom_bar(
+    stat = "identity",
+    position = "fill"
+  )  +
+  theme_scw() +
+  geom_hline(yintercept = 0, size = 1, colour="#333333") +
+  scale_y_continuous(name = "Percentage of Patients",
+                     labels = scales::label_percent()) +
+  scale_fill_diverging(discrete = TRUE) +
+  labs(
+    title = gppt_pcn_graph$question,
+    subtitle = gppt_pcn_graph$practice_name,
+    x = "Year"
+  )
+pcn_perc
+
