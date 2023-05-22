49a243b7ecb3da2fcc64073e1eabf5a54a9b4477
Paulj1989 <Paulj1989@users.noreply.github.com>
2023-05-22 18:11:31 +0000

    Style code (GHA)

--- data-raw/fft.R
+++ data-raw/fft.R
@@ -22,30 +22,31 @@ ods <- dplyr::rename(
   region_name = X8
 )
 
-fft<- fft_raw
+fft <- fft_raw
 fft <- dplyr::rename(
   fft_raw,
-  patient_registered_population =  patient_registered_population,
-  total_responses = 'Total Responses',
-  extremely_likely_very_good = 'Extremely Likely/Very Good',
- 	likely_good = 'Likely/Good',
- 	neither = 'Neither Likely nor Unlikely/Neither Good nor Poor',
- 	unlikely_poor = 'Unlikely/Poor',
-  extremely_unlikely_very_poor = 'Extremely Unlikely/Very Poor',
-  do_not_know = 'Do not Know',
-  perc_recommend = '% Recommend',
-  perc_not_recommend = '% Not Recommend'
+  patient_registered_population = patient_registered_population,
+  total_responses = "Total Responses",
+  extremely_likely_very_good = "Extremely Likely/Very Good",
+  likely_good = "Likely/Good",
+  neither = "Neither Likely nor Unlikely/Neither Good nor Poor",
+  unlikely_poor = "Unlikely/Poor",
+  extremely_unlikely_very_poor = "Extremely Unlikely/Very Poor",
+  do_not_know = "Do not Know",
+  perc_recommend = "% Recommend",
+  perc_not_recommend = "% Not Recommend"
 )
 
 fft_pivot <- fft %>%
-  pivot_longer(cols = !c(month, region_code, practice_code),
-               names_to = "answers",
-               values_to = "value"
+  pivot_longer(
+    cols = !c(month, region_code, practice_code),
+    names_to = "answers",
+    values_to = "value"
   )
 
 
 fft_merge <- fft_pivot %>%
-  left_join(ods,  by = join_by("practice_code"))
+  left_join(ods, by = join_by("practice_code"))
 
 fft_final <- fft_merge %>%
   select(
@@ -71,26 +72,29 @@ fft_final <- rename(
 
 fft_final <- fft_final %>%
   mutate(
-  total_flag = case_when(
-    str_detect(answers, "Total") ~ TRUE,
-    TRUE ~ FALSE
-  )) %>%
+    total_flag = case_when(
+      str_detect(answers, "Total") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) %>%
   mutate(
     popn_flag = case_when(
       str_detect(answers, "population") ~ TRUE,
       TRUE ~ FALSE
-  )) %>%
+    )
+  ) %>%
   mutate(
-  perc_recomm_flag = case_when(
-    str_detect(answers, "%") ~ TRUE,
-    TRUE ~ FALSE
-  )) %>%
+    perc_recomm_flag = case_when(
+      str_detect(answers, "%") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) %>%
   mutate(
-  sort_order = case_when(
-    answers == "Extremely Likely/Very Good" ~ 1, answers == "Likely/Good" ~ 2, answers == "Neither Likely nor Unlikely/Neither Good nor Poor" ~ 3,
-    answers == "Unlikely/Poor" ~ 4, answers == "Extremely Unlikely/Very Poor" ~ 5, answers == "Do not Know" ~ 6
+    sort_order = case_when(
+      answers == "Extremely Likely/Very Good" ~ 1, answers == "Likely/Good" ~ 2, answers == "Neither Likely nor Unlikely/Neither Good nor Poor" ~ 3,
+      answers == "Unlikely/Poor" ~ 4, answers == "Extremely Unlikely/Very Poor" ~ 5, answers == "Do not Know" ~ 6
+    )
   )
-)
 
 
 
