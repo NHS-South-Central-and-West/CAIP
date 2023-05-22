636db88196805c78c4aaf5b155fc8e08a84342d4
Johnson Ruth (NHS SCWCSU) <Ruth.Johnson@SCWCSU.NHS.UK>
2023-05-22 16:31:22 +0100

    Fixed order of graph stack and y axis labels

--- R/gppt_graph.R
+++ R/gppt_graph.R
@@ -1,3 +1,5 @@
+##Detailed Level GPPT Graphs
+
 #install.packages("devtools")
 #install.packages("tidyverse")
 #devtools::install_github("NHS-South-Central-and-West/scwplot")
@@ -11,10 +13,14 @@ library(scwplot)
 #### Practice Level Graphs
 ## practice_code and question_number to be dependent on the selected item on the app
 gppt_practice_graph <- filter(gppt_final, practice_code == "K84028",
-                              question_number == "Q04", summary_flag == FALSE,
+                              question_number == "Q21", summary_flag == FALSE,
                               total_flag == FALSE)
 
-practice_stack <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = value)) +
+
+#gppt_practice_graph <- gppt_practice_graph[order(gppt_practice_graph$sort_order),]
+
+
+practice_stack <- ggplot(gppt_practice_graph, aes(x = year, y = value, fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "stack"
@@ -22,6 +28,7 @@ practice_stack <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = 
   geom_hline(yintercept = 0, size = 1, colour = "#333333") +
   theme_scw() +
   scale_fill_diverging(discrete = TRUE) +
+  scale_y_continuous(labels = scales::label_number_si()) +
   labs(
     title = gppt_practice_graph$question,
     subtitle = gppt_practice_graph$practice_name,
@@ -30,7 +37,9 @@ practice_stack <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = 
   )
 practice_stack
 
-practice_perc <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = value)) +
+
+
+practice_perc <- ggplot(gppt_practice_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "fill"
@@ -47,20 +56,21 @@ practice_perc <- ggplot(gppt_practice_graph, aes(fill = answers, x = year, y = v
     subtitle = gppt_practice_graph$practice_name,
     x = "Year"
   )
+practice_perc
 
 
 #### PCN Level Graphs
 ## pcn_code and question_number to be dependent on the selected item on the app
 gppt_pcn_graph <- gppt_final %>%
-  select(pcn_code, pcn_name, question_number, summary_flag, confidence_flag,
-         total_flag, question, answers, year, value) %>%
-  filter(pcn_code == "U20931", question_number == "Q04", summary_flag == TRUE,
+  select(pcn_code, pcn_name, question_number, summary_flag,
+         total_flag, question, answers, year, value, sort_order) %>%
+  filter(pcn_code == "U20931", question_number == "Q32", summary_flag == FALSE,
         total_flag == FALSE) %>%
-  group_by(pcn_code, pcn_name, question, question_number, answers, year) %>%
+  group_by(pcn_code, pcn_name, question, question_number, answers, year, sort_order) %>%
   summarise(value = sum(value))
 
 
-pcn_stack <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
+pcn_stack <- ggplot(gppt_pcn_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "stack"
@@ -68,6 +78,7 @@ pcn_stack <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
   geom_hline(yintercept = 0, size = 1, colour = "#333333") +
   theme_scw() +
   scale_fill_diverging(discrete = TRUE) +
+  scale_y_continuous(labels = scales::label_number_si()) +
   labs(
     title = gppt_pcn_graph$question,
     subtitle = gppt_pcn_graph$pcn_name,
@@ -76,7 +87,7 @@ pcn_stack <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
   )
 pcn_stack
 
-pcn_perc <- ggplot(gppt_pcn_graph, aes(fill = answers, x = year, y = value)) +
+pcn_perc <- ggplot(gppt_pcn_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "fill"
@@ -98,15 +109,15 @@ pcn_perc
 #### ICB Level Graphs
 ## icb_code and question_number to be dependent on the selected item on the app
 gppt_icb_graph <- gppt_final %>%
-  select(icb_code, icb_name, question_number, summary_flag, confidence_flag,
-         total_flag, question, answers, year, value) %>%
+  select(icb_code, icb_name, question_number, summary_flag,
+         total_flag, question, answers, year, value, sort_order) %>%
   filter(icb_code == "QU9", question_number == "Q01", summary_flag == FALSE,
          total_flag == FALSE) %>%
-  group_by(icb_code, icb_name, question, question_number, answers, year) %>%
+  group_by(icb_code, icb_name, question, question_number, answers, year, sort_order) %>%
   summarise(value = sum(value))
 
 
-icb_stack <- ggplot(gppt_icb_graph, aes(fill = answers, x = year, y = value)) +
+icb_stack <- ggplot(gppt_icb_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "stack"
@@ -114,6 +125,7 @@ icb_stack <- ggplot(gppt_icb_graph, aes(fill = answers, x = year, y = value)) +
   geom_hline(yintercept = 0, size = 1, colour = "#333333") +
   theme_scw() +
   scale_fill_diverging(discrete = TRUE) +
+  scale_y_continuous(labels = scales::label_number_si()) +
   labs(
     title = gppt_icb_graph$question,
     subtitle = gppt_icb_graph$icb_name,
@@ -122,7 +134,7 @@ icb_stack <- ggplot(gppt_icb_graph, aes(fill = answers, x = year, y = value)) +
   )
 icb_stack
 
-icb_perc <- ggplot(gppt_icb_graph, aes(fill = answers, x = year, y = value)) +
+icb_perc <- ggplot(gppt_icb_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "fill"
@@ -144,15 +156,15 @@ icb_perc
 #### Region Level Graphs
 ## region_code and question_number to be dependent on the selected item on the app
 gppt_region_graph <- gppt_final %>%
-  select(region_code, region_name, question_number, summary_flag, confidence_flag,
-         total_flag, question, answers, year, value) %>%
+  select(region_code, region_name, question_number, summary_flag,
+         total_flag, question, answers, year, value, sort_order) %>%
   filter(region_code == "Y59", question_number == "Q01", summary_flag == FALSE,
          total_flag == FALSE) %>%
-  group_by(region_code, region_name, question, question_number, answers, year) %>%
+  group_by(region_code, region_name, question, question_number, answers, year, sort_order) %>%
   summarise(value = sum(value))
 
 
-region_stack <- ggplot(gppt_region_graph, aes(fill = answers, x = year, y = value)) +
+region_stack <- ggplot(gppt_region_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "stack"
@@ -160,6 +172,7 @@ region_stack <- ggplot(gppt_region_graph, aes(fill = answers, x = year, y = valu
   geom_hline(yintercept = 0, size = 1, colour = "#333333") +
   theme_scw() +
   scale_fill_diverging(discrete = TRUE) +
+  scale_y_continuous(labels = scales::label_number_si()) +
   labs(
     title = gppt_region_graph$question,
     subtitle = gppt_region_graph$region_name,
@@ -168,7 +181,7 @@ region_stack <- ggplot(gppt_region_graph, aes(fill = answers, x = year, y = valu
   )
 region_stack
 
-region_perc <- ggplot(gppt_region_graph, aes(fill = answers, x = year, y = value)) +
+region_perc <- ggplot(gppt_region_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "fill"
@@ -191,14 +204,14 @@ region_perc
 
 gppt_national_graph <- gppt_final %>%
   select(question_number, summary_flag,
-         total_flag, question, answers, year, value) %>%
+         total_flag, question, answers, year, value, sort_order) %>%
   filter(question_number == "Q01", summary_flag == FALSE,
          total_flag == FALSE) %>%
-  group_by(question, question_number, answers, year) %>%
+  group_by(question, question_number, answers, year, sort_order) %>%
   summarise(value = sum(value))
 
 
-national_stack <- ggplot(gppt_national_graph, aes(fill = answers, x = year, y = value)) +
+national_stack <- ggplot(gppt_national_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "stack"
@@ -206,6 +219,7 @@ national_stack <- ggplot(gppt_national_graph, aes(fill = answers, x = year, y = 
   geom_hline(yintercept = 0, size = 1, colour = "#333333") +
   theme_scw() +
   scale_fill_diverging(discrete = TRUE) +
+  scale_y_continuous(labels = scales::label_number_si()) +
   labs(
     title = gppt_national_graph$question,
     subtitle = "National Results",
@@ -214,7 +228,7 @@ national_stack <- ggplot(gppt_national_graph, aes(fill = answers, x = year, y = 
   )
 national_stack
 
-national_perc <- ggplot(gppt_national_graph, aes(fill = answers, x = year, y = value)) +
+national_perc <- ggplot(gppt_national_graph, aes(x = year, y = value,fill = reorder(answers,sort_order))) +
   geom_bar(
     stat = "identity",
     position = "fill"
