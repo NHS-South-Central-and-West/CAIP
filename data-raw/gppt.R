5354c39c2eb251ba815774f0ccc8e3b015904991
Paul Johnson <paul.johnson50@nhs.net>
2023-05-24 20:44:07 +0100

    Refactor GPPT data code

--- data-raw/gppt.R
+++ data-raw/gppt.R
@@ -1,134 +1,99 @@
-## code to prepare `gppt` dataset goes here
+## Script to Import & Wrangle GP Patient Survey Data
 
-gppt_raw <- readr::read_csv(
-  "O://T&C//BI Consultancy//A - Projects//UoM - SCW - PC CAP//Raw Data//gppt.csv",
-  col_names = FALSE, skip = 1
-)
-
-gppt <- dplyr::rename(
-  gppt_raw,
-  practice_code = X1,
-  practice_name = X2,
-  pcn_code = X3,
-  pcn_name = X4,
-  icb_code = X5,
-  icb_name = X6,
-  region_code = X7,
-  region_name = X8,
-  source_question_number = X9,
-  question_number = X10,
-  question_description = X11,
-  value = X12,
-  year = X13
-)
-
-#### Format questions
-library(stringr)
-library(dplyr)
-
-questions <- unique(gppt[c("source_question_number", "question_number", "question_description")])
-
-questions <- questions %>%
-  mutate(
-    question = case_when(
-      questions$question_number == "Q01" ~ "Q01 - Ease of getting through to someone at GP practice on the phone",
-      questions$question_number == "Q04" ~ "Q04 - Ease of using GP practice's website to look for information or access services",
-      questions$question_number == "Q16" ~ "Q16 - Satisfaction with type of appointment offered",
-      questions$question_number == "Q21" ~ "Q21 - Overall experience of making an appointment",
-      questions$question_number == "Q32" ~ "Q32 - Overall experience of GP practice"
-    )
-  ) %>%
-  mutate(
-    summary_flag = case_when(
-      str_detect(question_description, "Summary") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  #  mutate(
-  #    confidence_flag = case_when(
-  #      str_detect(question_description, "confidence") ~ TRUE,
-  #      TRUE ~ FALSE
-  #    )
-  #  ) %>%
-  mutate(
-    total_flag = case_when(
-      str_detect(question_description, "Total") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  mutate(
-    answers = sapply(strsplit(questions$question_description, " - ", fixed = TRUE), tail, 1)
-  ) %>%
-  mutate(
-    sort_order = case_when(
-      answers == "Very easy" ~ 1, answers == "Fairly easy" ~ 2, answers == "Not very easy" ~ 3,
-      answers == "Not at all easy" ~ 4, answers == "Haven't tried" ~ 5, answers == "Very good" ~ 1,
-      answers == "Fairly good" ~ 2, answers == "Neither good nor poor" ~ 3,
-      answers == "Fairly poor" ~ 4, answers == "Very poor" ~ 5,
-      answers == "Yes, and I accepted an appointment" ~ 1,
-      answers == "No, but I still took an appointment" ~ 2,
-      answers == "No, and I did not take an appointment" ~ 3,
-      answers == "I was not offered an appointment" ~ 4
-    )
+# import raw data from file
+gppt_raw <-
+  readr::read_csv(
+    "O://T&C//BI Consultancy//A - Projects//UoM - SCW - PC CAP//Raw Data//gppt.csv",
+    col_names = FALSE, skip = 1,
+    # specify encoding for fixing how apostrophes are processed
+    locale = readr::locale(encoding = "ISO-8859-1")
+  ) |>
+  dplyr::rename(
+    practice_code = X1,
+    practice_name = X2,
+    pcn_code = X3,
+    pcn_name = X4,
+    icb_code = X5,
+    icb_name = X6,
+    region_code = X7,
+    region_name = X8,
+    source_question_number = X9,
+    question_number = X10,
+    question_description = X11,
+    value = X12,
+    year = X13
   )
 
-
-#### Merge to final data
-
-gppt_merge <- gppt %>%
-  left_join(questions, by = "source_question_number")
-
-gppt_final <- gppt_merge %>%
-  select(
-    practice_code,
-    practice_name,
-    pcn_code,
-    pcn_name,
-    icb_code,
-    icb_name,
-    region_code,
-    region_name,
-    source_question_number,
-    question_number.x,
-    question,
-    answers,
-    summary_flag,
-    #    confidence_flag,
-    total_flag,
-    year,
-    value,
-    sort_order
-  )
-
-
-gppt_final <- rename(
-  gppt_final,
-  question_number = question_number.x
-)
-
-
-#### Creation of summary dataset
-gppt_summary <- gppt_final %>%
-  filter(summary_flag == FALSE, total_flag == FALSE) %>%
-  mutate(
-    calc_group = case_when(
-      answers == "Very easy" ~ 1, answers == "Fairly easy" ~ 1, answers == "Not very easy" ~ 2,
-      answers == "Not at all easy" ~ 2, answers == "Very good" ~ 3,
-      answers == "Fairly good" ~ 3,
-      answers == "Fairly poor" ~ 4, answers == "Very poor" ~ 4,
-      answers == "Yes, and I accepted an appointment" ~ 5,
-      answers == "No, but I still took an appointment" ~ 6,
-      answers == "No, and I did not take an appointment" ~ 6
+gppt <-
+  gppt_raw |>
+  # dplyr::distinct(source_question_number, .keep_all = TRUE) |>
+  dplyr::mutate(
+    question_description = stringr::str_replace_all(
+      question_description, "ï¿½", "'"
+    ),
+    question_type = dplyr::case_when(
+      stringr::str_detect(question_description, "Summary") ~ "summary",
+      stringr::str_detect(question_description, "Total") ~ "total",
+      .default = "response"
     )
-  ) %>%
-  mutate(
-    summary_desc = case_when(
-      calc_group == 1 ~ "Good", calc_group == 2 ~ "Poor", calc_group == 3 ~ "Good",
-      calc_group == 4 ~ "Poor", calc_group == 5 ~ "Satisfied", calc_group == 6 ~ "Dissatisfied"
+  ) |>
+  # filter for response counts only
+  # reconstruct summary/total from response if necessary
+  dplyr::filter(question_type == "response") |>
+  tidyr::separate_wider_regex(
+    question_description, c(question = ".*", "-", answer = ".*"),
+    too_few = "align_start"
+  ) |>
+  dplyr::mutate(
+    question = stringr::str_squish(question),
+    question = glue::glue("{question_number} - {question}"),
+    answer = stringr::str_squish(answer),
+    # create standardised/consistent ordinal scale for responses
+    response_scale =
+      dplyr::case_when(
+        answer %in% c(
+          "Very easy", "Very good",
+          "Yes, and I accepted an appointment"
+        ) ~ 1,
+        answer %in% c("Fairly easy", "Fairly good") ~ 2,
+        answer %in% c("Neither good nor poor") ~ 3,
+        answer %in% c(
+          "Not very easy", "Fairly poor",
+          "No, but I still took an appointment"
+        ) ~ 4,
+        answer %in% c(
+          "Not at all easy", "Very poor",
+          "No, and I did not take an appointment"
+        ) ~ 5,
+        # non-response treated as NA
+        answer %in% c(
+          "Haven't tried",
+          "I was not offered an appointment"
+        ) ~ NA,
+        .default = NA
+      ),
+    response_summary = dplyr::case_when(
+      answer %in% c(
+        "Very easy", "Fairly easy",
+        "Very good", "Fairly good"
+      ) ~ "Good",
+      answer %in% c("Neither good nor poor") ~ "Neither Good Nor Poor",
+      answer %in% c(
+        "Not very easy", "Not at all easy",
+        "Fairly poor", "Very poor"
+      ) ~ "Poor",
+      answer %in% c("Yes, and I accepted an appointment") ~ "Satisfied",
+      answer %in% c(
+        "No, but I still took an appointment",
+        "No, and I did not take an appointment"
+      ) ~ "Dissatisfied",
+      # non-response treated as NA
+      answer %in% c(
+        "Haven't tried",
+        "I was not offered an appointment"
+      ) ~ NA,
+      .default = NA
     )
   )
-
-
-
 
 usethis::use_data(gppt, overwrite = TRUE)
