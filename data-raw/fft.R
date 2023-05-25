2ebfcb3e14f9ee27ea55d93bfab6909a58b1dd74
Ruth Johnson <59695041+ruthjohnson78@users.noreply.github.com>
2023-05-25 17:04:43 +0100

    Tidied code for extracting data

--- data-raw/fft.R
+++ data-raw/fft.R
@@ -1,5 +1,7 @@
 ## code to prepare `fft` dataset goes here
 
+library(tidyverse)
+
 fft_raw <- readxl::read_excel(
   "O://T&C//BI Consultancy//A - Projects//UoM - SCW - PC CAP//Raw Data//GP FFT Data.xlsx"
 )
@@ -22,33 +24,40 @@ ods <- dplyr::rename(
   region_name = X8
 )
 
-fft <- fft_raw
-fft <- dplyr::rename(
-  fft_raw,
-  patient_registered_population = patient_registered_population,
-  total_responses = "Total Responses",
-  extremely_likely_very_good = "Extremely Likely/Very Good",
-  likely_good = "Likely/Good",
-  neither = "Neither Likely nor Unlikely/Neither Good nor Poor",
-  unlikely_poor = "Unlikely/Poor",
-  extremely_unlikely_very_poor = "Extremely Unlikely/Very Poor",
-  do_not_know = "Do not Know",
-  perc_recommend = "% Recommend",
-  perc_not_recommend = "% Not Recommend"
-)
-
-fft_pivot <- fft %>%
+fft_pivot <- fft_raw |>
   pivot_longer(
     cols = !c(month, region_code, practice_code),
     names_to = "answers",
     values_to = "value"
+  ) |>
+  left_join(ods, by = join_by("practice_code")) |>
+  mutate(
+    total_flag = case_when(
+      str_detect(answers, "Total") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) |>
+  mutate(
+    popn_flag = case_when(
+      str_detect(answers, "population") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) |>
+  mutate(
+    perc_recomm_flag = case_when(
+      str_detect(answers, "%") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) |>
+  mutate(
+    sort_order = case_when(
+      answers == "Extremely Likely/Very Good" ~ 1, answers == "Likely/Good" ~ 2, answers == "Neither Likely nor Unlikely/Neither Good nor Poor" ~ 3,
+      answers == "Unlikely/Poor" ~ 4, answers == "Extremely Unlikely/Very Poor" ~ 5, answers == "Do not Know" ~ 6
+    )
   )
 
-
-fft_merge <- fft_pivot %>%
-  left_join(ods, by = join_by("practice_code"))
-
-fft_final <- fft_merge %>%
+fft_final <-  subset(fft_pivot,popn_flag == FALSE & total_flag == FALSE &
+                      perc_recomm_flag ==FALSE)  |>
   select(
     practice_code,
     practice_name,
@@ -60,7 +69,8 @@ fft_final <- fft_merge %>%
     region_name,
     month,
     answers,
-    value
+    value,
+    sort_order
   )
 
 
@@ -69,32 +79,6 @@ fft_final <- rename(
   region_code = region_code.x
 )
 
-
-fft_final <- fft_final %>%
-  mutate(
-    total_flag = case_when(
-      str_detect(answers, "Total") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  mutate(
-    popn_flag = case_when(
-      str_detect(answers, "population") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  mutate(
-    perc_recomm_flag = case_when(
-      str_detect(answers, "%") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  mutate(
-    sort_order = case_when(
-      answers == "Extremely Likely/Very Good" ~ 1, answers == "Likely/Good" ~ 2, answers == "Neither Likely nor Unlikely/Neither Good nor Poor" ~ 3,
-      answers == "Unlikely/Poor" ~ 4, answers == "Extremely Unlikely/Very Poor" ~ 5, answers == "Do not Know" ~ 6
-    )
-  )
 
 
 usethis::use_data(fft, overwrite = TRUE)
