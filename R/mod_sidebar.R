63254b6a052d9f8ccd9e5c8dd6c9591a2418d683
Paul Johnson <paul.johnson50@nhs.net>
2023-05-27 05:10:38 +0100

    Add reactivity between modules

--- R/mod_sidebar.R
+++ R/mod_sidebar.R
@@ -25,61 +25,35 @@ mod_sidebar_ui <- function(id) {
       selectInput(
         inputId = ns("level"),
         label = "Level",
-        choices = c("National", "Regional", "ICB", "PCN", "Practice"),
+        choices = c("National", "Regional", "ICB", "PCN", "GP Practice"),
         selected = "National"
       ),
       shinyjs::hidden(
-        selectInput(
+        selectizeInput(
           inputId = ns("region"),
           label = "Region",
-          choices = c(
-            "All",
-            "Y63 - North East and Yorkshire",
-            "Y60 - Midlands",
-            "Y58 - South West"
-          ),
-          selected = "All"
-        )
+          choices = NULL
+          )
       ),
       shinyjs::hidden(
-        selectInput(
+        selectizeInput(
           inputId = ns("icb"),
           label = "ICB",
-          choices = c(
-            "All",
-            "QOQ - Humber and North Yorkshire",
-            "QHL - Birmingham and Solihull",
-            "QK1 - Leicester, Leicestershire and Rutland"
-          ),
-          selected = "All"
+          choices = NULL
         )
       ),
       shinyjs::hidden(
-        selectInput(
+        selectizeInput(
           inputId = ns("pcn"),
           label = "PCN",
-          choices = c(
-            "All",
-            "U49944 - Upper Don Valley",
-            "U52297 - Sunderland East",
-            "U41928 - Solihull Rural",
-            "U81960 - St John's Wood & Maida Vale"
-          ),
-          selected = "All"
+          choices = NULL
         )
       ),
       shinyjs::hidden(
-        selectInput(
+        selectizeInput(
           inputId = ns("practice"),
           label = "GP Practice",
-          choices = list(
-            "All",
-            "K84003 - Islip Surgery",
-            "K84042 - Woodstock Surgery",
-            "K84028 - Banbury Cross Health Centre"
-          ),
-          selected = "All",
-          multiple = FALSE
+          choices = NULL
         )
       ),
       tags$p("If you would like to compare an organisation (Region, ICB, PCN,
@@ -122,15 +96,29 @@ mod_sidebar_ui <- function(id) {
 #' @param input,output,session Internal parameters for {shiny}.
 #'
 #' @noRd
-mod_sidebar_server <- function(id, r) {
+mod_sidebar_server <- function(id, data) {
   moduleServer(
     id = id,
     function(input, output, session) {
       ns <- session$ns
 
-
       observeEvent(input$level, {
-        if (input$level != "National") {
+        if (input$level == "Regional") {
+          shinyjs::show("region", anim = TRUE, animType = "fade", time = .2)
+          shinyjs::hide("icb", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::hide("pcn", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::hide("practice", anim = TRUE, animType = "slide", time = 1.2)
+        } else if (input$level == "ICB") {
+          shinyjs::show("region", anim = TRUE, animType = "fade", time = .2)
+          shinyjs::show("icb", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::hide("pcn", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::hide("practice", anim = TRUE, animType = "slide", time = 1.2)
+        } else if (input$level == "PCN") {
+          shinyjs::show("region", anim = TRUE, animType = "fade", time = .2)
+          shinyjs::show("icb", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::show("pcn", anim = TRUE, animType = "slide", time = 1.2)
+          shinyjs::hide("practice", anim = TRUE, animType = "slide", time = 1.2)
+        } else if (input$level == "GP Practice") {
           shinyjs::show("region", anim = TRUE, animType = "fade", time = .2)
           shinyjs::show("icb", anim = TRUE, animType = "slide", time = 1.2)
           shinyjs::show("pcn", anim = TRUE, animType = "slide", time = 1.2)
@@ -142,6 +130,147 @@ mod_sidebar_server <- function(id, r) {
           shinyjs::hide("practice", anim = TRUE, animType = "slide", time = 1.2)
         }
       })
+
+      updateSelectizeInput(
+        session,
+       "region",
+        choices = c(
+          "ALL" = "",
+          data |> dplyr::distinct(region) |> dplyr::pull()
+          ),
+        server = TRUE
+        )
+
+      updateSelectizeInput(
+        session,
+       "icb",
+        choices = c(
+          "ALL" = "",
+          data |> dplyr::distinct(icb) |> dplyr::pull()
+          ),
+        server = TRUE
+        )
+
+      updateSelectizeInput(
+        session,
+       "pcn",
+        choices = c(
+          "ALL" = "",
+          data |> dplyr::distinct(pcn) |> dplyr::pull()
+          ),
+        server = TRUE
+      )
+
+      updateSelectizeInput(
+        session,
+       "practice",
+        choices = c(
+          "ALL" = "",
+          data |> dplyr::distinct(practice) |> dplyr::pull()
+          ),
+        server = TRUE
+      )
+
+      gppt_data <- reactive({
+        if (input$level == "National") {
+          data |>
+            dplyr::filter(
+              question_number == "Q32",
+              !is.na(response_scale)) |>
+            dplyr::summarise(
+              value = sum(value),
+              .by = c(year, question, question_number, answer, response_scale)
+            )
+        } else {
+          data |>
+            dplyr::filter(
+              question_number == "Q32",
+              conditional(input$region != "", region == input$region),
+              conditional(input$icb != "", icb == input$icb),
+              conditional(input$pcn != "", pcn == input$pcn),
+              conditional(input$practice != "", practice == input$practice),
+              !is.na(response_scale)
+            ) |>
+            dplyr::select(
+              region, icb, pcn, practice,
+              question_number, question, answer, value,
+              year, response_scale
+            )
+        }
+      })
+
+      output$download_data <- downloadHandler(
+        filename = function() {
+          # Use the selected dataset as the suggested file name
+          paste0(input$level, "_gppt_data.csv")
+        },
+        content = function(file) {
+          # Write the dataset to the `file` that will be downloaded
+          readr::write_csv(gppt_data(), file)
+        }
+      )
+
+      # plot_title <- reactive({ input$question })
+
+      plot_subtitle <- reactive({
+        if (input$level == "National") {
+          "AGGREGATED NATIONAL AVERAGE"
+        } else if (input$level == "Regional") {
+          input$region
+        } else if (input$level == "ICB") {
+          input$icb
+        } else if (input$level == "PCN") {
+          input$pcn
+        } else if (input$level == "GP Practice") {
+          input$practice
+        }
+      })
+
+      gppt_plot <- reactive({
+        gppt_plot <- gppt_data() |>
+          ggplot2::ggplot(ggplot2::aes(
+            x = factor(year), y = value,
+            fill = stats::reorder(answer, response_scale))
+          ) +
+          ggplot2::geom_bar(
+            stat = "identity", position = "fill",
+            colour = "#333333", linewidth = 1
+          ) +
+          ggplot2::geom_hline(yintercept = 0, linewidth = 1, colour = "#333333") +
+          ggplot2::scale_y_continuous(labels = scales::label_percent()) +
+          scwplot::theme_scw(base_size = 12) +
+          scwplot::scale_fill_diverging(reverse = FALSE, discrete = TRUE) +
+          ggplot2::labs(
+            # title = plot_title(),
+            subtitle = plot_subtitle(),
+            x = NULL, y = "% Respondents"
+          )
+
+        return(gppt_plot)
+      })
+
+      output$download_plot <- downloadHandler(
+        filename = function() {
+          # Use the selected dataset as the suggested file name
+          paste0(input$level, "_gppt_plot.png")
+        },
+        content = function(file) {
+          # Write the dataset to the `file` that will be downloaded
+
+          ggplot2::ggsave(file, gppt_plot(), width = 12, height = 9, dpi = 320)
+        }
+      )
+
+      return(
+        list(
+          level = reactive({ input$level }),
+          region = reactive({ input$region }),
+          icb = reactive({ input$icb }),
+          pcn = reactive({ input$pcn }),
+          practice = reactive({ input$practice })
+        )
+      )
+
     }
   )
 }
