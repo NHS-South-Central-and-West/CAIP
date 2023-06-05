4b56dcc68a5aba960bac2d3ffc7b833e28e1bb5c
Paul Johnson <paul.johnson50@nhs.net>
2023-06-05 20:17:46 +0100

    Add reset_monthday function

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,37 @@
-#' helpers
+#' Conditional function for handling default inputs for filters
 #'
-#' @description A utils function
+#' @description A utility function for reducing reliance on many nested if
+#' statements in the app's filtering process. Where default values (\code{""})
+#' are returned in one of the app's \code{selectizeInput()} boxes, this function
+#' returns \bold{TRUE} so that dplyr's \code{filter()} doesn't filter that
+#' column.
 #'
-#' @return The return value, if any, from executing the utility.
+#' @param condition The condition to be evaluated, returning \bold{TRUE} or
+#' \bold{FALSE}
+#' @param success The result to be returned if the condition returns \bold{TRUE}
+#'
+#' @return If \code{condition = TRUE} then returns \bold{success}, else returns
+#' \bold{TRUE}.
 #'
 #' @noRd
 conditional <- function(condition, success) {
   if (condition) success else TRUE
+}
+
+#' Set the date returned by the date range inputs in the app as the first day of
+#' that month.
+#'
+#' @description A utility function for ensuring that the date range returned
+#' includes any data for the month selected.
+#'
+#' @param date A date object
+#'
+#' @return \code{date} The first day of the selected month (for example,
+#' selecting June 2023 returns \code{"2023/06/01}).
+#'
+#' @noRd
+reset_monthday <- function(date) {
+  date <- as.POSIXlt(date)
+  date$mday <- 1
+  as.Date(date)
 }
