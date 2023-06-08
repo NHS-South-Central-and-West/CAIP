8ede72844e1cafd5539d444cf6c9975fbb89980c
Paul Johnson <paul.johnson50@nhs.net>
2023-06-08 17:21:19 +0100

    Add contact links

--- R/mod_downloads.R
+++ R/mod_downloads.R
@@ -12,42 +12,63 @@ mod_downloads_ui <- function(id) {
     column(
       width = 12,
       align = "left",
-      tags$br(),
-      tags$p(
+      br(),
+      p(
         "To download the plots or the data shown in the tab you are currently
         viewing, use the ",
         fontawesome::fa_i(name = "download", fill_opacity = 1),
         " buttons in the main panel."
       ),
-      tags$br(),
-      #   tags$br(),
-      #   tags$p(
-      #     "For a detailed report about the organisation you have selected in the
-      #     dropdown filters above, including national comparisons and comparisons
-      #     with similar organisations, use the download button below:"
-      #   ),
-      #   tags$br()
+      br(),
+      # br(),
+      # p("For a detailed report about the organisation you have selected in the
+      #   dropdown filters above, including national comparisons and comparisons
+      #   with similar organisations, use the download button below:"),
+      # br()
       # ),
       # column(
       #   width = 12,
       #   align = "center",
-      #   downloadButton(outputId = ns("download_report"), label = "Download Report")
+      #   downloadButton(outputId = ns("download_report"),
+      #                  label = "Download Report")
     ),
     column(
       width = 12,
       align = "center",
       offset = 0.5,
-      tags$hr(),
-      tags$b("This app is in active development. Features will be added and
-               bugs will be squashed.",
-        style = "color:#B82A4E"
+      hr(),
+      p("This app is in active development. Features will be added and
+        bugs will be squashed.",
+        style = "color:#B82A4E; font-weight: bold;"
       ),
-      tags$br(),
-      tags$br(),
-      tags$b("If you spot any errors, however, please let us know!",
-        style = "color:#B82A4E"
+      br(),
+      br(),
+      p("If you spot any errors, however, please let us know:",
+        style = "color:#B82A4E; font-weight: bold;"
+      )
+    ),
+    column(
+      width = 6,
+      align = "center",
+      br(),
+      p(class = "fa-regular fa-envelope", style = "color:#B82A4E"),
+      a("Email",
+        target = "_blank",
+        href = "mailto:scwcsu.primarycaresupport@nhs.net",
+        style = "color:#B82A4E; font-weight: bold;"
+      )
+    ),
+    column(
+      width = 6,
+      align = "center",
+      br(),
+      p(class = "fa-brands fa-github", style = "color:#B82A4E"),
+      a("GitHub",
+        target = "_blank",
+        href = "https://github.com/NHS-South-Central-and-West/CAIP/issues",
+        style = "color:#B82A4E; font-weight: bold;"
       ),
-      tags$br()
+      br()
     )
   )
 }
