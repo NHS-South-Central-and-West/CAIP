985bf792e7463c0b3d943b8dd0577144b4349f7a
Paulj1989 <Paulj1989@users.noreply.github.com>
2023-06-14 14:01:16 +0000

    Style code (GHA)

--- tests/testthat/test-fct_fft_data.R
+++ tests/testthat/test-fct_fft_data.R
@@ -1,10 +1,10 @@
 test_that("returns correct number of columns", {
-  date_from = "2021-01-01"
-  date_to = "2023-01-01"
-  region = "Y59 - SOUTH EAST COMMISSIONING REGION"
-  icb = "QHL - BIRMINGHAM AND SOLIHULL ICB"
-  pcn = "U53896 - NORTH LEWISHAM PCN"
-  practice = "F86025 - OAK TREE MEDICAL CENTRE"
+  date_from <- "2021-01-01"
+  date_to <- "2023-01-01"
+  region <- "Y59 - SOUTH EAST COMMISSIONING REGION"
+  icb <- "QHL - BIRMINGHAM AND SOLIHULL ICB"
+  pcn <- "U53896 - NORTH LEWISHAM PCN"
+  practice <- "F86025 - OAK TREE MEDICAL CENTRE"
 
   expect_equal(ncol(get_fft_national_data(date_from, date_to)), 4)
   expect_equal(ncol(get_fft_region_data(date_from, date_to, region)), 5)
