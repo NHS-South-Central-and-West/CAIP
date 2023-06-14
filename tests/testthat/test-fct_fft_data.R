b753739bf8d033e3979d440e7ccbecfbca5ef443
Paul Johnson <paul.johnson50@nhs.net>
2023-06-14 14:44:47 +0100

    Clean up data functions testing

--- tests/testthat/test-fct_fft_data.R
+++ tests/testthat/test-fct_fft_data.R
@@ -1,53 +1,14 @@
-test_that("check number of columns in fft national data", {
-  fft_national_test <-
-    get_fft_national_data(
-      date_from = "2021-01-01",
-      date_to = "2023-01-01"
-    )
+test_that("returns correct number of columns", {
+  date_from = "2021-01-01"
+  date_to = "2023-01-01"
+  region = "Y59 - SOUTH EAST COMMISSIONING REGION"
+  icb = "QHL - BIRMINGHAM AND SOLIHULL ICB"
+  pcn = "U53896 - NORTH LEWISHAM PCN"
+  practice = "F86025 - OAK TREE MEDICAL CENTRE"
 
-  expect_equal(ncol(fft_national_test), 4)
-})
-
-test_that("check number of columns in fft regional data", {
-  fft_region_test <-
-    get_fft_region_data(
-      date_from = "2021-01-01",
-      date_to = "2023-01-01",
-      org = "Y59 - SOUTH EAST COMMISSIONING REGION"
-    )
-
-  expect_equal(ncol(fft_region_test), 5)
-})
-
-test_that("check number of columns in fft ICB data", {
-  fft_icb_test <-
-    get_fft_icb_data(
-      date_from = "2021-01-01",
-      date_to = "2023-01-01",
-      org = "QHL - BIRMINGHAM AND SOLIHULL ICB"
-    )
-
-  expect_equal(ncol(fft_icb_test), 5)
-})
-
-test_that("check number of columns in fft PCN data", {
-  fft_pcn_test <-
-    get_fft_icb_data(
-      date_from = "2021-01-01",
-      date_to = "2023-01-01",
-      org = "U53896 - NORTH LEWISHAM PCN"
-    )
-
-  expect_equal(ncol(fft_pcn_test), 5)
-})
-
-test_that("check number of columns in fft practice data", {
-  fft_practice_test <-
-    get_fft_icb_data(
-      date_from = "2021-01-01",
-      date_to = "2023-01-01",
-      org = "F86025 - OAK TREE MEDICAL CENTRE"
-    )
-
-  expect_equal(ncol(fft_practice_test), 5)
+  expect_equal(ncol(get_fft_national_data(date_from, date_to)), 4)
+  expect_equal(ncol(get_fft_region_data(date_from, date_to, region)), 5)
+  expect_equal(ncol(get_fft_icb_data(date_from, date_to, icb)), 5)
+  expect_equal(ncol(get_fft_pcn_data(date_from, date_to, pcn)), 5)
+  expect_equal(ncol(get_fft_practice_data(date_from, date_to, practice)), 5)
 })
